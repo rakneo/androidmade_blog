82bb5737301eb29d4553eadec390df46bc92fa2d
rakneo <rv.g6598@outlook.com>
2016-10-29 16:13:43 +0530

    Update on Views

--- androidmade_site/urls.py
+++ androidmade_site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from androidmade_site.blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list, name='post_list'),
