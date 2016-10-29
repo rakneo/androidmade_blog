5cd270da5a60e23530f198d93be99cbeefd3ab7d
rakneo <rv.g6598@outlook.com>
2016-10-29 18:13:04 +0530

    Update on Views rev 2 import error

--- androidmade_site/urls.py
+++ androidmade_site/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
-from androidmade_site import blog
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', blog.views.post_list, name='post_list'),
+    url(r'', include('blog.urls')),
 
 ]
