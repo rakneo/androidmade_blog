88348f77270c32f22bb21e36a339f65a037a9041
rakneo <rv.g6598@outlook.com>
2016-10-29 17:09:16 +0530

    Update on Views

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+#def post_list(request):
+#    return render(request, 'blog/post_list.html', {})
+#
