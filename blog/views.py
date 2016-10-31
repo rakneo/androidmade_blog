c9435a1cddb1f51b2be74b42c514d3fbf9f24469
rakneo <rv.g6598@outlook.com>
2016-10-31 12:30:58 +0530

    views.py error corrected

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import  post
 # Create your views here.
 
 def post_list(request):
-    posts = post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
+    posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
